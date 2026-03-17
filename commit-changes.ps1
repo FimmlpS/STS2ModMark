50ef5d5094e52c746aa29a6b2f5d8f0f2710b022
FimmlpS <1224688749@qq.com>
2026-03-17 20:51:18 +0800

    Fix avatar and background image paths

---
+++ commit-changes.ps1
@@ -0,0 +1,6 @@
+git add .
+themes/kira/layout/components/header.ejs
+themes/kira/layout/components/sidebar.ejs
+themes/kira/layout/layout.ejs
+git commit -m "Fix avatar and background image paths"
+git push
---
+++ trigger-workflow.ps1
@@ -0,0 +1,2 @@
+git commit --allow-empty -m "Trigger GitHub Actions workflow"
+git push
